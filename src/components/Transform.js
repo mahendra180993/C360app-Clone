12a8fc4fc358773b34e4c258acc007f15dd46d7f
mahendra reddy <marellamahe@gmail.com>
2021-12-13 16:23:11 +0530

    react insitial commit

---
+++ src/components/Transform.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Transform() {
+    return (
+        <div>
+            	<div class="transrow">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<h4 class="wow fadeInUp animated">Tranform Your Enterprise</h4> </div>
+				<div class="col-md-3"><a href="#" class="wow fadeInUp animated">Talk with us</a> </div>
+				{/* <a href="#" class="wow fadeInUp animated" data-toggle="modal" data-target="#get_trained"></a> */}
+			</div>
+		</div>
+        </div>
+        </div>
+    )
+}
